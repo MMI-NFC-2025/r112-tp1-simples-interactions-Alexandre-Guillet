9c34c1e75219f223b103fb14be27cd96d07d58bc
Alexandre-Guillet <alexandre.guillet@edu.univ-fcomte.fr>
2025-11-07 16:32:48 +0100

    js fini

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,14 @@
-// TODO
+const Rouge = document.querySelector('#a-mettre-en-rouge');
+Rouge.style.color = 'red';
+
+const BoutonRouge = document.querySelector('#en-rouge-suite-a-click');
+BoutonRouge.addEventListener('click', () => {
+    BoutonRouge.style.color = 'red';
+    });
+
+const Lesh2 = document.querySelectorAll('h2');
+Lesh2.forEach((elem) => {
+  elem.addEventListener('click', (evt) => {
+    evt.target.style.color = 'red';
+  });
+});
